22869f5aeeba5254244375517cb981935ff5ebc8
David Melo <gmkronox@gmail.com>
2024-02-12 17:17:58 -0500

    Agrega módulo de autenticación y configuración de JWT*** ***Agrega dependencias de bcrypt y passport-jwt*** ***Actualiza las dependencias de NestJS a la versión 10*** ***Agrega JWT_SECRET al archivo .env

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => {
+        return {
+          secret: process.env.JWT_SECRET,
+          signOptions: { expiresIn: '2h' },
+        };
+      },
+    }),
+  ],
+  exports: [JwtStrategy, TypeOrmModule, PassportModule, JwtModule],
+})
+export class AuthModule {}
